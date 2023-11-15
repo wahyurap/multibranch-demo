4ef4554774fd7576d16b785753f0fb4699f20cee
Wahyu_Rap <72025033+wahyurap@users.noreply.github.com>
2023-11-15 13:44:53 +0700

    Update lagi.php

--- lagi.php
+++ lagi.php
@@ -1,16 +1,1 @@
-<?php
-$title = "Standard User Page";
-$userrole = "Standard User"; // Allow only logged in users
-include "login/misc/pagehead.php";
-?>
-</head>
-<body>
-  <?php require 'login/misc/pullnav.php'; ?>
-    <div class="container">
-
-        <h2>Standard User Page</h2>
-        <p>Hello, <?=$_SESSION["username"]?>!</p>
-        <p>This page requires a Standard User to be logged in</p>
-    </div>
-</body>
-</html>
+<h1>test</h1>
